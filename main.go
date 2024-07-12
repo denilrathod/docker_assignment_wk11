ac141d9aeca7669110a7e67528a62aeac6b14981
meanstack9716 <meanstack9@gmail.com>
2024-07-11 20:07:40 -0400

    main function fro main.go file

--- main.go
+++ main.go
@@ -15,3 +15,16 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "This is the About page.")
 }
+
+// go mod init github.com/sojoudian/SimpleAPIServer
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/about", aboutHandler)
+
+	log.Println("Starting server on :3003")
+	err := http.ListenAndServe(":3003", mux)
+	if err != nil {
+		log.Fatalf("Error starting server: %s\n", err)
+	}
+}
